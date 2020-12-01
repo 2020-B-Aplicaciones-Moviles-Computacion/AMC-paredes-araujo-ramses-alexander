ae2e2ba1a34abe7abbf39d6ab050dc72c9bc7dbf
Ramses <ramses.paredes@epn.edu.ec>
2020-12-01 12:52:03 -0500

    Ciclo de Vida

--- AndroidStudioProjects/app/src/main/java/com/example/aplicacionmoviles/MainActivity.kt
+++ AndroidStudioProjects/app/src/main/java/com/example/aplicacionmoviles/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.aplicacionmoviles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonCicloVida = findViewById<Button>(R.id.button_ir_ciclo_vida)
+        buttonCicloVida.setOnClickListener {
+            irCicloVida()
+        }
+
+    }
+    fun irCicloVida(){
+        val intentExplicito = Intent(
+            this,
+            ACicloVida:: class.java
+        )
+        startActivity(intentExplicito)
     }
 }
